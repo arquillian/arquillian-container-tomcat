718e9231c6abf0ad1aa951dd2ca068febf6294c3
Juraj Huska <jhuska@redhat.com>
2011-08-16 17:10:56 +0200

    initial entry for tomcat-managed-7, not working, retrieveProtocolMetadata has problems

---
+++ tomcat-managed-7/src/main/java/org/jboss/arquillian/container/tomcat/managed_7/TomcatManagedExtension.java
@@ -0,0 +1,14 @@
+package org.jboss.arquillian.container.tomcat.managed_7;
+
+import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
+
+import org.jboss.arquillian.core.spi.LoadableExtension;
+
+public class TomcatManagedExtension implements LoadableExtension {
+
+	 @Override
+	   public void register(ExtensionBuilder builder)
+	   {
+	      builder.service(DeployableContainer.class, TomcatManagedContainer.class);
+	   }
+}
