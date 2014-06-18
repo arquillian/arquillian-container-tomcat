e60311545cf31dd1e33050d3a97ea1187a79bf1f
Stephen Coy <steve.coy@me.com>
2014-06-18 13:31:41 +1000

    ARQ-866 Add support for setting catalina.bash in Managed containers
    * Tomcat 5.5, 6, 7

--- tomcat-managed-6/src/main/java/org/jboss/arquillian/container/tomcat/managed_6/TomcatManagedConfiguration.java
+++ tomcat-managed-6/src/main/java/org/jboss/arquillian/container/tomcat/managed_6/TomcatManagedConfiguration.java
@@ -37,6 +37,8 @@ public class TomcatManagedConfiguration extends CommonTomcatConfiguration
 
    private String catalinaHome = System.getenv("CATALINA_HOME");
 
+   private String catalinaBase = System.getenv("CATALINA_BASE");
+
    private String javaHome = System.getenv("JAVA_HOME");
 
    private String javaVmArguments = "-Xmx512m -XX:MaxPermSize=128m";
@@ -57,6 +59,9 @@ public class TomcatManagedConfiguration extends CommonTomcatConfiguration
        // if no javaHome set, reuse this Java JVM
        if (javaHome == null || "".equals(javaHome)) {
            javaHome = System.getProperty("java.home");
+       }
+       if (catalinaBase == null || "".equals(catalinaBase)) {
+           catalinaBase = catalinaHome;
        }
    }
 
@@ -101,6 +106,14 @@ public class TomcatManagedConfiguration extends CommonTomcatConfiguration
    {
       this.catalinaHome = catalinaHome;
    }
+
+   public String getCatalinaBase() {
+        return catalinaBase;
+    }
+
+   public void setCatalinaBase(String catalinaBase) {
+        this.catalinaBase = catalinaBase;
+    }
 
    public String getJavaHome()
    {
